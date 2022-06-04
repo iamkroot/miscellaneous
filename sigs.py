2f642c11506f87c3afe5ad18e6b44e4609f9e5bd
iamkroot <kroot.patel@gmail.com>
2022-06-04 15:34:08 +0530

    Update sigs

--- sigs.py
+++ sigs.py
@@ -5,9 +5,9 @@ import random
 
 
 # PAT = re.compile(r"(?P<stem>.*)_Page(?P<pg_num>\d+)")
-PAT = re.compile(r"page_Page(?P<pg_num>\d+)")
-cmd = ["magick", "-density", "100", "{input}", "-grayscale", "average", "-attenuate", "0.4", "-rotate", "{rotation}", "+noise", "Gaussian", "{output}"]
-fold = Path(r"D:\Downloads") / "intern"
+PAT = re.compile(r"_(?P<pg_num>\d+)")
+cmd = ["magick", "-quality", "50", "{input}", "-grayscale", "average", "-attenuate", "0.4", "-rotate", "{rotation}", "+noise", "Gaussian", "{output}"]
+fold = Path(r"/home/kroot/Downloads/MSR_RF/")
 out_fold = fold / "out"
 out_fold.mkdir(exist_ok=True)
 for img in fold.iterdir():
@@ -18,6 +18,8 @@ for img in fold.iterdir():
         cmd[3] = str(img)
         rot = random.random() * 0.5 * (1, -1)[random.random() > 0.5]
         cmd[9] = f"{rot:.2f}"
-        cmd[12] = str(out_fold / (f"page-{m['pg_num']}_out.png"))
+        cmd[12] = str(out_fold / (f"page-{m['pg_num']}_out.jpg"))
+        # cmd[12] = f'"{cmd[12]}"'
         print(cmd)
-        sp.check_call(" ".join(cmd), shell=True)
+        # sp.check_call(" ".join(cmd), shell=True)
+        sp.check_call(cmd)
